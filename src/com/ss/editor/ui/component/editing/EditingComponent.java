541f4a5c83f58f16bb8a3480a5b793ad7cced2a4
javasabr <javasabr@gmail.com>
2017-02-08 07:10:22 +0300

    updated rlib

---
+++ src/com/ss/editor/ui/component/editing/EditingComponent.java
@@ -0,0 +1,7 @@
+package com.ss.editor.ui.component.editing;
+
+/**
+ * Created by ronn on 08.02.17.
+ */
+public interface EditingComponent {
+}
---
+++ src/com/ss/editor/ui/component/editing/EditingContainer.java
@@ -0,0 +1,7 @@
+package com.ss.editor.ui.component.editing;
+
+/**
+ * Created by ronn on 07.02.17.
+ */
+public class EditingContainer {
+}
