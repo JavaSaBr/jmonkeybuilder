cf3017738f663d0b895709af36981ac6d80448ec
JavaSaBr <javasabr@gmail.com>
2017-07-19 18:21:09 +0300

    implemented a starter to develop plugins.

--- src/main/java/com/ss/editor/DevelopPluginStarter.java
+++ src/main/java/com/ss/editor/DevelopPluginStarter.java
@@ -1,4 +1,21 @@
 package com.ss.editor;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * The start class to develop plugins.
+ *
+ * @author JavaSaBr
+ */
 public class DevelopPluginStarter {
+
+    public static void main(@NotNull final String[] args) throws IOException {
+        final Path embeddedFolder = Paths.get("./embedded-plugins");
+        System.setProperty("editor.embedded.plugins.path", embeddedFolder.toAbsolutePath().toString());
+        JFXApplication.main(args);
+    }
 }
